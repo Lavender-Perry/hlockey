ddf30b3feeb6f77b8d2314377735661cf43e49d6
Lavender Perry <endie2@protonmail.com>
2022-06-11 11:18:07 -0700

    Improve Team class, improve random player name generation

--- src/player.rb
+++ src/player.rb
@@ -1,23 +1,5 @@
 $random = Random.new 8010897121101114
 
-def player_name
-  vowels = ["a", "e", "i", "o", "u", "y"]
-  consonates = ("a".."z").filter do |l| not vowels.include? l end
-  fname_length = $random.rand(7) + 3
-  lname_length = $random.rand(7) + 3
-  name = ""
-  add_rand_letter = Proc.new do |i|
-    array = if i % 2 == 0 then consonates else vowels end
-    name += array[$random.rand array.length]
-  end
-
-  fname_length.times &add_rand_letter
-  name += " "
-  lname_length.times &add_rand_letter
-
-  name.split.map do |n| n.capitalize end.join " "
-end
-
 class Player
   attr_accessor :name, :offense, :defense, :agility
   def initialize
@@ -25,6 +7,36 @@ class Player
     @offense = $random.rand * 5
     @defense = $random.rand * 5
     @agility = $random.rand * 5
+  end
+end
+
+def player_name
+  fname_length = $random.rand(9) + 2
+  lname_length = $random.rand(9) + 2
+  fname = ""
+  lname = ""
+
+  fname_length.times do |i| fname += random_letter i end
+  lname_length.times do |i| lname += random_letter i end
+
+  fname.capitalize + " " + lname.capitalize
+end
+
+def random_letter(i)
+  array = random_letter_array i % 3
+  array[$random.rand array.length]
+end
+
+def random_letter_array(n)
+  vowels = ["a", "e", "i", "o", "u", "y"]
+  consonates = ("a".."z").filter do |l| not vowels.include? l end
+  case n
+  when 0
+    consonates
+  when 1
+    vowels
+  else
+    random_letter_array $random.rand 2
   end
 end
 
--- src/team.rb
+++ src/team.rb
@@ -1,9 +1,12 @@
 require_relative "./player.rb"
 
 class Team
-  attr_accessor :name, :offense, :defense
-  def initialize(name)
+  attr_accessor :name, :emoji, :wins, :losses, :offense, :defense
+  def initialize(name, emoji)
     @name = name
+    @emoji = emoji
+    @wins = 0
+    @losses = 0
     @offense = []
     @defense = []
     3.times do @offense << Player.new end

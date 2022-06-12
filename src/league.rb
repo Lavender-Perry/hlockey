16af39c2b16083c7282dd51fd6278d998d1c6f5a
Lavender Perry <endie2@protonmail.com>
2022-06-11 22:07:57 -0700

    Add League class

---
+++ src/league.rb
@@ -0,0 +1,36 @@
+require_relative "./team.rb"
+
+class League
+  attr_accessor :season, :divisions, :prng
+  def initialize(season)
+    @season = season
+    @divisions = {
+      "Wet Warm": [
+        Team.new("Antalya Pirates", "🌊"),
+        Team.new("Baden Hallucinations", "🍄"),
+        Team.new("Lagos Soup", "🥣"),
+        Team.new("Pica Acid", "🧪")
+      ],
+      "Dry Warm": [
+        Team.new("Erlangen Ohms", "🇴"),
+        Team.new("Pompei Eruptions", "🌋"),
+        Team.new("Rio de Janeiro Gamers", "💯"),
+        Team.new("Wyrzysk Rockets", "🚀")
+      ],
+      "Wet Cool": [
+        Team.new("Cape Town Transplants", "🌱"),
+        Team.new("Manbij Fish", "🐠"),
+        Team.new("Nice Backflippers", "🔙"),
+        Team.new("Orcadas Base Fog", "🌁")
+      ],
+      "Dry Cool": [
+        Team.new("Amsterdam Reapers", "💀"),
+        Team.new("Baghdad Abaci", "🧮"),
+        Team.new("Dawson City Impostors", "🔪"),
+        Team.new("Jakarta Architects", "📐")
+      ]
+    }
+    @prng = Random.new 69420 * season
+  end
+end
+

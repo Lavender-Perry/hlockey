27c1a633eb22df1501fdcdbcba6fae173e454e58
Lavender Perry <endie2@protonmail.com>
2022-06-15 16:07:12 -0700

    Markov chain for name generation

--- src/player.rb
+++ src/player.rb
@@ -1,3 +1,5 @@
+require_relative "./data/names_markov_chain.rb"
+
 class Player
   attr_accessor :name, :offense, :defense, :agility
 
@@ -12,28 +14,20 @@ class Player
 
   private
   def random_name
-    vowels = ["a", "e", "i", "o", "u"]
-    consonates = ("a".."z").filter do |l| not vowels.include? l end
+    combination = "__"
+    next_letter = ""
+    result = ""
 
-    # Starting letter should be chosen randomly from every letter,
-    # so that each vowel isn't more likely to be the first letter than each consonate.
-    name = (vowels + consonates).sample random: @@prng
-
-    added_letters = @@prng.rand(7) + 1
-    return name.upcase + (vowels.include?(name) ? consonates : vowels)
-      .sample(random: @@prng) if added_letters == 1
-
-    # To avoid 3 vowels/consonates in a row:
-    # If the starting letter is a vowel, it should not be mode 0
-    # If it is a consonate, it should not be mode 2
-    mode = vowels.include?(name) ? @@prng.rand(3) + 1 : (@@prng.rand(3) + 3) % 4
-
-    added_letters.times do
-      name += (mode / 2 == 0 ? vowels : consonates).sample random: @@prng
-      mode = mode % 2 == 0 ? mode + 1 : @@prng.rand(2) + 3 - mode
+    10.times do
+      next_letters = $names_markov_chain[combination]
+      break if next_letters == nil
+      next_letter = next_letters.sample random: @@prng
+      break if next_letter == "_"
+      result += next_letter
+      combination = combination[1] + next_letter
     end
-
-    name.capitalize
+    
+    result.capitalize
   end
 end
 

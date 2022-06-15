f4c03e4780027bf3033ac1520559b8f20cc691c0
Lavender Perry <endie2@protonmail.com>
2022-06-15 11:59:27 -0700

    Improve random name generation

--- src/player.rb
+++ src/player.rb
@@ -12,26 +12,28 @@ class Player
 
   private
   def random_name
-    len = @@prng.rand(9) + 2
-    mod_num = len == 2 ? 2 : 3
-    i_add = @@prng.rand mod_num
-    name = ""
+    vowels = ["a", "e", "i", "o", "u"]
+    consonates = ("a".."z").filter do |l| not vowels.include? l end
 
-    len.times do |i| name += random_letter (i + i_add) % mod_num end
+    # Starting letter should be chosen randomly from every letter,
+    # so that each vowel isn't more likely to be the first letter than each consonate.
+    name = (vowels + consonates).sample random: @@prng
+
+    added_letters = @@prng.rand(7) + 1
+    return name.upcase + (vowels.include?(name) ? consonates : vowels)
+      .sample(random: @@prng) if added_letters == 1
+
+    # To avoid 3 vowels/consonates in a row:
+    # If the starting letter is a vowel, it should not be mode 0
+    # If it is a consonate, it should not be mode 2
+    mode = vowels.include?(name) ? @@prng.rand(3) + 1 : (@@prng.rand(3) + 3) % 4
+
+    added_letters.times do
+      name += (mode / 2 == 0 ? vowels : consonates).sample random: @@prng
+      mode = mode % 2 == 0 ? mode + 1 : @@prng.rand(2) + 3 - mode
+    end
 
     name.capitalize
   end
-
-  def random_letter n
-    vowels = ["a", "e", "i", "o", "u"]
-    consonates = ("a".."z").filter do |l| not vowels.include? l end
-    case n
-    when 0
-      consonates
-    when 1
-      vowels
-    else
-      @@prng.rand(2) == 0 ? consonates : vowels
-    end.sample random: @@prng
-  end
 end
+
